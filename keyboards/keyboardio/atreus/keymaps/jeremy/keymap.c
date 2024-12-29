cbf70f8aa3ca1b62695a2f2a930fcb59ec48873c
Jeremy Cowgar <jeremy@cowgar.com>
2024-12-29 12:24:52 -0500

    feat(atreus): add CAPS_LOCK binding

--- keyboards/keyboardio/atreus/keymaps/jeremy/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/jeremy/keymap.c
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_CLMK] = LAYOUT(                                                            
       KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,                         KC_J,     KC_L,     KC_U,     KC_Y,  KC_QUOT,  
       KC_A,     KC_R,     KC_S,     KC_T,     KC_G,                         KC_M,     KC_N,     KC_E,     KC_I,     KC_O,  
-      KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,    _______,  _______,   KC_K,     KC_H,  KC_COMM,   KC_DOT,  KC_SLSH,  
+      KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,    CW_TOGG,  _______,   KC_K,     KC_H,  KC_COMM,   KC_DOT,  KC_SLSH,
       KC_LGUI,  KC_LCTL,  KC_LALT,  MO(_NAV), KC_SPC,  JC_SBSP,  JC_SDEL,   JC_FET,MO(_NUM), KC_LALT,  KC_LCTL,  KC_LGUI
   ),
   [_NAV] = LAYOUT(                                                 
