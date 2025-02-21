9626aee841027e4e183247023b28e288560ccaed
Jeremy Cowgar <jeremy@cowgar.com>
2025-02-21 15:32:52 -0500

    feat(keychron/q8): Colemak and HRMs

--- keyboards/keychron/q8/ansi/keymaps/jeremy/keymap.c
+++ keyboards/keychron/q8/ansi/keymaps/jeremy/keymap.c
@@ -26,15 +26,25 @@ enum layers {
     _FN3
 };
 
+#define JC_A MT(MOD_LGUI, KC_A)
+#define JC_R MT(MOD_LALT, KC_R)
+#define JC_S MT(MOD_LCTL, KC_S)
+#define JC_T MT(MOD_LSFT, KC_T)
+
+#define JC_N MT(MOD_LSFT, KC_N)
+#define JC_E MT(MOD_LCTL, KC_E)
+#define JC_I MT(MOD_LALT, KC_I)
+#define JC_O MT(MOD_LGUI, KC_O)
+
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_INS,
-        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
-        KC_CAPS, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,           KC_HOME,
-        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
+        KC_TAB,  KC_Q,     KC_W,     KC_F,    KC_P,    KC_B,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_QUOT,  KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_PGUP,
+        KC_CAPS, JC_A,     JC_R,     JC_S,    JC_T,    KC_G,              KC_M,    JC_N,    JC_E,    JC_I,     JC_O,     KC_SCLN,  KC_ENT,           KC_PGDN,
+        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_D,    KC_V,     KC_UNDS, KC_K,    KC_H,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
         KC_LCTL, KC_LOPT,  KC_LCMD,           KC_SPC,           MO(_FN1), MO(_FN3),         KC_SPC,            KC_RCMD,            KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_ansi_69(
